9ed3325a3c651cf06429c37bcf51fb821b3ae178
Wlaia <wellingtonlaialopes@gmail.com>
2026-02-18 07:32:29 -0300

    chore: remove sections after differentials

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
 import { Hero } from "@/components/home/Hero";
 import { Differentials } from "@/components/home/Differentials";
-import { HowItWorks } from "@/components/home/HowItWorks";
-import { FeaturedCourses } from "@/components/home/FeaturedCourses";
-import { MaterialsSection } from "@/components/home/MaterialsSection";
-import { SocialProof } from "@/components/home/SocialProof";
-
 import { UpcomingCourse } from "@/components/home/UpcomingCourse";
+
+
 
 export function Home() {
     return (
@@ -13,10 +10,6 @@ export function Home() {
             <Hero />
             <UpcomingCourse />
             <Differentials />
-            <FeaturedCourses />
-            <HowItWorks />
-            <MaterialsSection />
-            <SocialProof />
         </div>
     );
 }
